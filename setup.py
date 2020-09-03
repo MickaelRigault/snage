37a5a856c521d086b01be418f73166fcd9a8e1c6
MickaelRigault <mick.rigault@gmail.com>
2020-09-03 19:13:14 +0200

    missing the setup

---
+++ setup.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+#
+
+DESCRIPTION = "snprop"
+LONG_DESCRIPTION = """ Underlying SN Ia Property distributions """
+
+DISTNAME = 'snprop'
+AUTHOR = 'Mickael Rigault'
+MAINTAINER = 'Mickael Rigault' 
+MAINTAINER_EMAIL = 'm.rigault@ipnl.in2p3.fr'
+URL = 'https://github.com/MickaelRigault/snprop/'
+LICENSE = 'Apache 2.0'
+DOWNLOAD_URL = 'https://github.com/MickaelRigault/snprop/tarball/0.2'
+VERSION = '0.2.0'
+
+try:
+    from setuptools import setup, find_packages
+    _has_setuptools = False
+except ImportError:
+    from distutils.core import setup
+
+
+def check_dependencies():
+    install_requires = []
+
+    # Just make sure dependencies exist, I haven't rigorously
+    # tested what the minimal versions that will work are
+    # (help on that would be awesome)
+
+    return install_requires
+
+if __name__ == "__main__":
+
+    install_requires = check_dependencies()
+
+    if _has_setuptools:
+        packages = find_packages()
+        print(packages)
+    else:
+        # This should be updated if new submodules are added
+        packages = ['snprop']
+
+        
+    setup(name=DISTNAME,
+          author=AUTHOR,
+          author_email=MAINTAINER_EMAIL, 
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          install_requires=install_requires,
+          scripts=[],
+          packages=packages,
+          include_package_data=True,
+          classifiers=[
+              'Intended Audience :: Science/Research',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3.5',              
+              'License :: OSI Approved :: BSD License',
+              'Topic :: Scientific/Engineering :: Astronomy',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Operating System :: MacOS'],
+      )
